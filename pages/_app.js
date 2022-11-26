d8799ae375a59246ee19ceed5ec3e2490df482f9
Gabriel Bezerra <104681677+GabrielBezerraG@users.noreply.github.com>
2022-11-26 16:10:16 -0300

    Added title and lang attribute

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import '../src/styles/reset.css';
 import { GlobalStyles } from '../src/styles/GlobalStyles';
+import Head from 'next/head'
 
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Frontend Mentor | E-commerce Product Page</title>
+      </Head>
       <GlobalStyles />
       <Component {...pageProps} />
     </>
